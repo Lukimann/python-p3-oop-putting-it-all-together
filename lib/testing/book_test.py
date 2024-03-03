24e175aad4e5fb93a2af2d639ecd093d3a6ea698
lukimann <lukewanyama5@gmail.com>
2024-03-03 16:31:19 +0300

    Done

--- lib/testing/book_test.py
+++ lib/testing/book_test.py
@@ -5,7 +5,10 @@ from book import Book
 import io
 import sys
 
+
+
 class TestBook:
+
     '''Book in book.py'''
 
     def test_has_title_and_page_count(self):
@@ -15,13 +18,14 @@ class TestBook:
         assert(book.title == "And Then There Were None")
 
     def test_requires_int_page_count(self):
-        '''prints "page_count must be an integer" if page_count is not an integer.'''
-        book = Book("And Then There Were None", 272)
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        book.page_count = "not an integer"
-        sys.stdout = sys.__stdout__
-        assert captured_out.getvalue() == "page_count must be an integer\n"
+         '''prints "page_count must be an integer" if page_count is not an integer.'''
+         book = Book("And Then There Were None", 272)
+         captured_out = io.StringIO()
+         sys.stdout = captured_out
+         book.page_count = "not an integer"
+         sys.stdout = sys.__stdout__
+         assert captured_out.getvalue() == "page_count must be an integer\n"
+        
 
     def test_can_turn_page(self):
         '''outputs "Flipping the page...wow, you read fast!" when method turn_page() is called'''
